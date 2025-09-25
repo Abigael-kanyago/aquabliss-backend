87a86ad8552ce1c2df4c75dcc7e5fa36afcdc88c
Abigael <wanjikuwamwangi9@gmail.com>
2025-09-25 10:29:14 +0300

    Update DB to Supabase

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, 
 });
 
 pool.connect()
-  .then(() => console.log("✅ Connected to PostgreSQL"))
-  .catch(err => console.error("❌ Database connection error:", err));
+  .then(() => console.log(" Connected to Supabase PostgreSQL"))
+  .catch(err => console.error(" Database connection error:", err));
 
 module.exports = pool;
+
